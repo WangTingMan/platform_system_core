4c313a045b0816cb095921004dfc08f36d37361e
WangTingZe <kingpin58@aliyun.com>
2025-10-13 23:02:33 +0800

    Support more definitions in direct:x

--- libutils/include/utils/direct.h
+++ libutils/include/utils/direct.h
@@ -16,6 +16,11 @@
 #define __DIRENT64_INO_T uint64_t /* Historical accident. */
 #endif
 
+/* Block device */
+#if !defined(S_IFBLK)
+#  define S_IFBLK 0
+#endif
+
 #ifndef DT_UNKNOWN
 #define DT_UNKNOWN 0
 #define DT_FIFO S_IFIFO
@@ -30,6 +35,34 @@
 
 #if !defined(PATH_MAX)
 #define PATH_MAX 260
+#endif
+
+/*
+ * File type macros.  Note that block devices, sockets and links cannot be
+ * distinguished on Windows and the macros S_ISBLK, S_ISSOCK and S_ISLNK are
+ * only defined for compatibility.  These macros should always return false
+ * on Windows.
+ */
+#if !defined(S_ISFIFO)
+#  define S_ISFIFO(mode) (((mode)&S_IFMT) == S_IFIFO)
+#endif
+#if !defined(S_ISDIR)
+#  define S_ISDIR(mode) (((mode)&S_IFMT) == S_IFDIR)
+#endif
+#if !defined(S_ISREG)
+#  define S_ISREG(mode) (((mode)&S_IFMT) == S_IFREG)
+#endif
+#if !defined(S_ISLNK)
+#  define S_ISLNK(mode) (((mode)&S_IFMT) == S_IFLNK)
+#endif
+#if !defined(S_ISSOCK)
+#  define S_ISSOCK(mode) (((mode)&S_IFMT) == S_IFSOCK)
+#endif
+#if !defined(S_ISCHR)
+#  define S_ISCHR(mode) (((mode)&S_IFMT) == S_IFCHR)
+#endif
+#if !defined(S_ISBLK)
+#  define S_ISBLK(mode) (((mode)&S_IFMT) == S_IFBLK)
 #endif
 
 /* Multi-byte character version */
